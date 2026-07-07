7dfa624775a944a78fc1eb62f211bf1eba413b9c
manurkarshri <manurkar.shri@gmail.com>
2026-07-07 13:52:20 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="city-emergency-alerts-v5";
-const FILES=["./","./index.html","./404.html","./manifest.json","./css/base.css","./css/layout.css","./css/cards.css","./css/responsive.css","./js/app.js","./js/config.js","./js/utils.js","./js/render.js","./js/dashboard.js","./js/static-ui.js","./js/pwa.js","./data/live-alerts.json","./data/weather.json","./data/status.json","./data/localities.json","./assets/icons/icon.svg"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;const url=new URL(e.request.url);if(url.origin===location.origin){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));return}e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
