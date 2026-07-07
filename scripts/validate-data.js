9b51b7bc0d4a2261196c0aaabef2f7846be64ef0
manurkarshri <manurkar.shri@gmail.com>
2026-07-07 15:46:32 +0530

    Delete scripts/validate-data.js

--- scripts/validate-data.js
+++
@@ -1,7 +0,0 @@
-import fs from "fs/promises";
-const files=["data/live-alerts.json","data/weather.json","data/status.json","data/localities.json"];
-for(const f of files){
-  const txt=await fs.readFile(f,"utf8");
-  JSON.parse(txt);
-  console.log("valid",f);
-}
